1ed70afaba885b31237704c988502f703c8955af
Phan Tien Tai <taip7547@gmail.com>
2025-06-03 22:02:15 +0700

    feat: Update DTO (Add Validation And EmployeeRegisterDTO)

--- services/employee-service/src/main/java/com/example/employee_service/dto/EmployeeLoginDTO.java
+++ services/employee-service/src/main/java/com/example/employee_service/dto/EmployeeLoginDTO.java
@@ -1,15 +1,18 @@
 package com.example.employee_service.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Value;
 
 @Value
 public class EmployeeLoginDTO {
 
     @NotBlank(message = "Username is required")
+    @Size(min = 6, max = 15, message = "Username must not smaller than 6 and greater than 15 character!")
     String username;
 
     @NotBlank(message = "Password is required")
+    @Size(min = 6, max = 64, message = "Password must not smaller than 6 and greater than 64 character!")
     String password;
 
 }
---
+++ services/employee-service/src/main/java/com/example/employee_service/dto/EmployeeRegisterDTO.java
@@ -0,0 +1,57 @@
+package com.example.employee_service.dto;
+
+import jakarta.validation.constraints.*;
+import lombok.Value;
+
+import java.time.LocalDate;
+
+@Value
+public class EmployeeRegisterDTO {
+
+    @NotBlank(message = "Name is required")
+    @Size(min = 3, max = 50, message = "Name must not smaller than 3 and greater than 50 character!")
+    String name;
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 6, max = 15, message = "Username must not smaller than 6 and greater than 15 character!")
+    String username;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, max = 64, message = "Password must not smaller than 6 and greater than 64 character!")
+    String password;
+
+    @NotBlank(message = "Confirm Password is required")
+    String confirmPassword;
+
+    @NotNull(message = "Date of birth is required")
+    LocalDate DOB;
+
+    @NotBlank(message = "Phone number is required")
+    @Pattern(regexp = "\\d{10}", message = "Phone number must be exactly 10 digits")
+    String phone_number;
+
+    @Email(message = "Email must be valid")
+    @NotBlank(message = "Email is required")
+    @Size(min = 10, max = 50, message = "Email must not smaller than 10 and greater than 50 character!")
+    String email;
+
+    @NotBlank(message = "Address is required")
+    @Size(min = 10, max = 100, message = "Address must not smaller than 10 and greater than 100 character!")
+    String address;
+
+    @NotNull(message = "Salary is required")
+    @DecimalMin(value = "1.0", message = "Salary must be at least 1.0")
+    Float salary;
+
+    @NotNull(message = "Team ID is required")
+    @Min(value = 1, message = "Team Id not smaller than 1!")
+    Integer team_id;
+
+    @AssertTrue(message = "Passwords do not match")
+    public boolean isPasswordConfirmed() {
+        if (password == null || confirmPassword == null) return false;
+        return password.equals(confirmPassword);
+    }
+
+}
+
