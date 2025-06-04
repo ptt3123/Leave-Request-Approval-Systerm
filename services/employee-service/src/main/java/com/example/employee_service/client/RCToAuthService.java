5777d3b056729a511ffcec4076421a28d3dc09ac
Phan Tien Tai <taip7547@gmail.com>
2025-06-04 07:33:21 +0700

    fix: Fix Client( Add @Conponent for Spring Injection

--- services/employee-service/src/main/java/com/example/employee_service/client/RCToAuthService.java
+++ services/employee-service/src/main/java/com/example/employee_service/client/RCToAuthService.java
@@ -2,11 +2,12 @@ package com.example.employee_service.client;
 
 import com.example.employee_service.dto.JWTData;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
+@Component
 public class RCToAuthService {
 
     private final RestClient authRestClient;
@@ -19,10 +20,10 @@ public class RCToAuthService {
 
     public ResponseEntity<String> generateJWT(JWTData data) {
         return authRestClient.post()
-                .uri("generate-jwt")
+                .uri("/generate-jwt")
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(data)
                 .retrieve()
-                .body(new ParameterizedTypeReference<ResponseEntity<String>>() {});
+                .toEntity(String.class);
     }
 }
